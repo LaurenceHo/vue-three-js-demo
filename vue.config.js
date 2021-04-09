cb753bed854852e50220e4380a87fceb97eb1fa5
Laurence Ho <bluegray.he@aol.com>
2021-04-10 08:29:18 +1200

    Initial Three.js and component

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    open: true,
+    disableHostCheck: true,
+    historyApiFallback: true,
+    hot: true,
+    port: 8080,
+  },
+  lintOnSave: true,
+  productionSourceMap: false,
+};
